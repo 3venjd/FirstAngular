d5c2b3db7f2d38dc5c1d91f5fa82aebf1ed092f4
luis jimenez <eve_ghetto@hotmail.com>
2023-09-08 19:22:08 -0500

    improves

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -20,6 +20,8 @@ GetCompany(company:string){
 }
 */
 
+inputCompany = "Microsoft"
+ 
 enabledInput = false;
 
 userRegistered = false;
@@ -41,6 +43,7 @@ setRegistreredUser(event:Event){
   }
 
 }
+
 
 constructor() {}
 
